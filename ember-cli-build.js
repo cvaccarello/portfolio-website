9ea9f10913da9b9eac21cd8d8ea0d8f9d4b763fc
Chris <cvaccarello@brandgage.com>
2023-07-14 17:55:12 -0400

    Updated files to new formatted prettier, eslint, and editorconfig settings

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,9 +3,17 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
-    // Add options here
-  });
+	const app = new EmberApp(defaults, {
+		// 'ember-font-awesome': {
+		// 	useScss: true
+		// },
+		// sassOptions: {
+		// 	includePaths: [
+		// 		'app',
+		// 		'bower_components/breakpoint-sass/stylesheets'
+		// 	]
+		// }
+	});
 
-  return app.toTree();
+	return app.toTree();
 };
